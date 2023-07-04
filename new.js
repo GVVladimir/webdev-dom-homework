cab7f22ceeaf1941bad8d9da3d874b74a4a41df4
Vladimir <vladimir.grishaew@yandex.ru>
2023-07-04 15:38:14 +0300

    API1

--- new.js
+++ new.js
@@ -5,23 +5,42 @@ const commentTextElement = document.getElementById('add-form-text');
 const commentLikeCounterElements = document.querySelectorAll('.likes-counter');
 const likebuttonElements = document.querySelectorAll('.like-button');
 
-const comments =[
-    {
-        name:'Глеб Фокин ',
-        data: `12.02.22 12:18`,
-        comment: 'Это будет первый комментарий на этой странице',
-        likesNum: '3',
-        isLike: false,
-        },
-        {
-        name:'Варвара Н.',
-        data: `13.02.22 19:22`,
-        comment: 'Мне нравится как оформлена эта страница! ❤',
-        likesNum: '75',
-        isLike: false,
-        } 
+const result = fetch('https://wedev-api.sky.pro/api/v1/:grishaev-vladimir/comments',
+{
+  method: "GET",
+});
+
+result.then((response) => {
+  const jsonResult = response.json();
+  jsonResult.then((responseData) => {
+   
+    comments = responseData.comments;
+    renderComments();
+  })
+});
+
+let comments = [
+    // {
+    //     name:'Глеб Фокин ',
+    //     data: `12.02.22 12:18`,
+    //     comment: 'Это будет первый комментарий на этой странице',
+    //     likesNum: '3',
+    //     isLike: false,
+    //     isEdit: true,
+    //     button:'Редактировать'
+    //     },
+    //     {
+    //     name:'Варвара Н.',
+    //     data: `13.02.22 19:22`,
+    //     comment: 'Мне нравится как оформлена эта страница! ❤',
+    //     likesNum: '75',
+    //     isLike: false,
+    //     isEdit: true,
+    //     button:'Редактировать'
+    //     } 
     
 ];
+
 // счетчик лайков
 const calculLikeSum = () => {
   const likebuttons = document.querySelectorAll('.like-button');
@@ -31,18 +50,44 @@ const calculLikeSum = () => {
        event.stopPropagation()
          const el =  likebutton.dataset.el;  
  
-         if(comments[el].isLike){  
-          comments[el].likesNum --;
-          comments[el].isLike = false;  
+         if(comments[el].isLiked){  
+          comments[el].likes --;
+          comments[el].isLiked = false;  
         } else {
-          comments[el].likesNum ++;
-          comments[el].isLike = true;
+          comments[el].likes ++;
+          comments[el].isLiked = true;
         }
 
      renderComments()
        });  
    }; 
 };
+
+// смена названия кнопки переписать
+
+const newNameButton = () => {
+  const nameButtonElements = document.querySelectorAll('.add-form-button');
+  for (const nameButtonElement of nameButtonElements){
+    nameButtonElement.addEventListener('click', (event) => {
+      const el = nameButtonElement.dataset.namebtn;
+       event.stopPropagation()
+    
+      if (comments[el].isEdit){
+        comments[el].comment = `<textarea  type="textarea" class='add-form-text data-texta=${el}' rows="4">${comments.comment}</textarea>`;
+        comments[el].isEdit = false;
+        comments[el].button = 'Cохранить'
+
+        } else if (comments[el].isEdit === false) {
+          comments[el].comment;
+        comments[el].isEdit = true
+        comments[el].button = 'Редактировать'
+        
+      }
+        renderComments();
+      
+    })
+  }
+}
 
 // не активная кнопка
 const buttonEctiv = () => {
@@ -61,7 +106,7 @@ const answerComment = () => {
     textComment.addEventListener('click', () => {
       
       const el = textComment.dataset.el;
-      commentTextElement.value = `${'>' + ' ' + comments[el].comment + '  ' + comments[el].name + ':' + '  '}`;
+      commentTextElement.value = `${'>' + ' ' + comments[el].text + '  ' + comments[el].author + ':' + '  '}`;
      
     }) 
   }
@@ -77,69 +122,64 @@ const renderComments = () => {
     const commentsHtml = comments.map((comment, el) => {
         return `<li data-el="${el}" id="comment" class="comment">
         <div class="comment-header">
-          <div>${comment.name}</div>
-          <div>${comment.data}</div>
+          <div>${comment.author.name}</div>
+          <div>${comment.date}</div>
         </div>
         <div data-el="${el}" class="comment-body" >
-          <div   class="comment-text"  type ="form">
-           ${comment.comment}
-          </div>
+          <div   class="comment-text">${comment.text}</div>
         </div>
         <div class="comment-footer">
           <div class="likes">
-            <span   class="likes-counter">${comment.likesNum}</span>
-            <button  data-el="${el}" class="like-button ${comment.isLike ? "-active-like" : ""}"></button>
+            <span   class="likes-counter">${comment.likes}</span>
+            <button  data-el="${el}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
           </div>
         </div>
       </li>`
     }).join('');
+   
 
     commentsLinkElement.innerHTML = commentsHtml;
     calculLikeSum();
     answerComment();
     buttonEctiv();
-  
+    // newNameButton();
 };
 
 renderComments();
 
 butttonWriteElement.addEventListener('click', () => {
    
-   comments.push({
-    name:commentNameElevent.value
-    .replace('<', '&lt')
-    .replace('>', '&gt'),
-        data: new Date().toLocaleDateString().slice(0, -2) + ' ' + new Date().toLocaleTimeString().slice(0, -3),
-        comment: commentTextElement.value,
-        likesNum: 0,
-        isLike: false,
-   })
-//    commentsLinkElement.innerHTML = commentsLinkElement.innerHTML + `<li id="comment" class="comment">
-//             <div class="comment-header">
-//             <div>${commentNameElevent.value}</div>
-//             <div>${new Date().toLocaleDateString().slice(0, -2) + ' ' + new Date().toLocaleTimeString().slice(0, -3)}</div>
-//             </div>
-//             <div class="comment-body">
-//             <div class="comment-text">
-//             ${commentTextElement.value}
-//             </div>
-//             </div>
-//             <div class="comment-footer">
-//             <div class="likes">
-//                 <span class="likes-counter">0</span>
-//                 <button  class="like-button"></button>
-//             </div>
-//             </div>
-//         </li>
-//         `
+  //  comments.push({
+  //   name:commentNameElevent.value
+  //   .replace('<', '&lt')
+  //   .replace('>', '&gt'),
+  //       data: new Date().toLocaleDateString().slice(0, 6) + new Date().toLocaleDateString().slice(8, 10) + ' ' + new Date().toLocaleTimeString().slice(0, -3),
+  //       comment: commentTextElement.value,
+  //       likesNum: 0,
+  //       isLike: false,
+  //       isEdit: true,
+  //       button: 'Редактировать'
+  //  })
 
+   fetch('https://wedev-api.sky.pro/api/v1/:grishaev-vladimir/comments',{
+  method: "POST",
+  body: JSON.stringify({
+    text: commentTextElement.value,
+    name:  commentNameElevent,  
+  }),
+}).then((response) => {
+ 
+  response.json().then((responseData) => {
+   
+    comments = responseData.comments;
+   console.log(comments);
+  })
+  renderComments()
+});
+   
 
-
-// colorLikeSum();
+   
 butttonWriteElement.disabled = true;
-
-renderComments();
-
 
   commentNameElevent.value = '';
   commentTextElement.value = '';
